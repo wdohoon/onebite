a8d79e9bbd8d0a9ffb4654fac4fe8a5db457e268
wdohoon <dnjsehgns98@gmail.com>
2024-09-20 15:16:34 +0900

    2024.09.20

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,14 +1,11 @@
 import './App.css';
-import {useState} from "react";
-import Bulb from "./components/Bulb";
-import Counter from "./components/Counter";
+import Register from "./components/Register";
 
 function App() {
 
     return (
         <>
-            <Bulb/>
-            <Counter/>
+        <Register />
         </>
     )
 }
