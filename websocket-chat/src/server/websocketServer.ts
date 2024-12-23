f60ad10c4baf6a484d04bc81127c9778e0af5dd7
원도훈 <dnjsehgns98@gmail.com>
2024-12-23 18:13:40 +0900

    test

--- websocket-chat/src/server/websocketServer.ts
+++ websocket-chat/src/server/websocketServer.ts
@@ -5,9 +5,9 @@ interface ExtendedWebSocket extends WebSocket {
 }
 
 const wss = new WebSocketServer({ port: 3001 });
+// Next.js 기본 포트가 3000이므로, WebSocket 서버는 3001로 예시를 들었습니다.
 
 let userCount = 0;
-// Next.js는 기본적으로 3000 포트를 사용하므로, WebSocket 서버는 3001 포트로 열어둡니다.
 
 wss.on('connection', (ws: ExtendedWebSocket) => {
   // 사용자를 구분하기 위한 id
@@ -17,11 +17,12 @@ wss.on('connection', (ws: ExtendedWebSocket) => {
   // 클라이언트에서 받은 메시지를 전체 사용자에게 broadcast
   ws.on('message', (data: string) => {
     const messageData = JSON.parse(data);
-    console.log(`[WebSocket] 메시지 수신 from ${ws.id}: `, messageData);
+    console.log(`[WebSocket] 메시지 수신 from ${ws.id}:`, messageData);
 
     // 전체 클라이언트에게 메시지를 전송
     wss.clients.forEach((client) => {
-      if (client.readyState === 1) {
+      // readyState 값이 OPEN(=1)인지 확인
+      if (client.readyState === WebSocket.OPEN) {
         client.send(
           JSON.stringify({
             id: ws.id,
